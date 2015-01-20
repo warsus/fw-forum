714b853855228bfb105103e95b149755efbad459
warsus <zerstroyer@googlemail.com>
2015-01-20 22:50:01 +0100

    dont use quotes for tempid in code

--- src/f/db.clj
+++ src/f/db.clj
@@ -40,47 +40,48 @@
 
 (defn load-schema [conn]
   (let [schema
-        [{:db/id #db/id[:db.part/db]
+        [{:db/id (d/tempid :db.part/db)
           :db/ident :beitrag/text
           :db/valueType :db.type/string
           :db/fulltext true
           :db/cardinality :db.cardinality/one
           :db/doc "Beitrag text"
           :db.install/_attribute :db.part/db}
-         {:db/id #db/id[:db.part/db]
+         {:db/id (d/tempid :db.part/db)
           :db/ident :beitrag/datum
           :db/valueType :db.type/instant
+          :db/index true
           :db/cardinality :db.cardinality/one
           :db/doc "Beitrag text"
           :db.install/_attribute :db.part/db}
-         {:db/id #db/id[:db.part/db]
+         {:db/id (d/tempid :db.part/db)
           :db/ident :beitrag/titel
           :db/fulltext true
           :db/valueType :db.type/string
           :db/cardinality :db.cardinality/one
           :db/doc "Beitrag titel"
           :db.install/_attribute :db.part/db}
-         {:db/id #db/id[:db.part/db]
+         {:db/id (d/tempid :db.part/db)
           :db/ident :beitrag/user
           :db/valueType :db.type/string
           :db/fulltext true
           :db/cardinality :db.cardinality/one
           :db/doc "Beitrag titel"
           :db.install/_attribute :db.part/db}
-         {:db/id #db/id[:db.part/db]
+         {:db/id (d/tempid :db.part/db)
           :db/ident :beitrag/zeit
           :db/valueType :db.type/instant
           :db/cardinality :db.cardinality/one
           :db/doc "Beitrag Zeit"
           :db.install/_attribute :db.part/db}
-         {:db/id #db/id[:db.part/db]
+         {:db/id (d/tempid :db.part/db)
           :db/ident :beitrag/antworten
           :db/valueType :db.type/ref
           :db/cardinality :db.cardinality/many
           :db/index true
           :db/doc "Antworten"
           :db.install/_attribute :db.part/db}
-         {:db/id #db/id[:db.part/db]
+         {:db/id (d/tempid :db.part/db)
           :db/ident :beitrag/id
           :db/index true
           :db/unique :db.unique/identity
@@ -88,7 +89,7 @@
           :db/cardinality :db.cardinality/one
           :db/doc "id"
           :db.install/_attribute :db.part/db}
-         {:db/id #db/id[:db.part/db]
+         {:db/id (d/tempid :db.part/db)
           :db/ident :beitrag/spam
           :db/index true
           :db/valueType :db.type/boolean
@@ -98,50 +99,28 @@
     @(d/transact conn schema)))
 
 (defn qbeitrag
-  ([db] (q {:find '[?b ?titel ?user ?id ?text]
-            :where '[[?b beitrag/id ?id]
-                     [?b beitrag/titel ?titel]
-                     [?b beitrag/user ?user]
-                     [?b beitrag/text ?text]]} db))
-  ([db id] (q {:find '[?b ?titel ?user ?id ?text]
+  ([db] (q {:find '[?b]
+            :where '[[?b beitrag/id ?id]]} db))
+  ([db id] (q {:find '[?b]
                :in   '[$ ?id]
-               :where  '[[?b beitrag/id ?id]
-                         [?b beitrag/titel ?titel]
-                         [?b beitrag/user ?user]
-                         [?b beitrag/text ?text]]} db id)))
+               :where  '[[?b beitrag/id ?id]]} db id)))
 
 (defn bids->id [db bids]
-  (q {:find '[?id]
-      :in '[$ [?bid]]
-      :where '[[?id :beitrag ?bid]]} db bids))
-
-(defn qbeitragbyid [db id]
-  (let [e (d/entity db id)]
-    [(get e :beitrag/id) (get e :beitrag/titel) (get e :beitrag/user)]))
+  (map #(:db/id (d/entity db [:beitrag/id %])) bids))
 
 (defn qbeitragvon ([db user]
-                     (q {:find '[?user ?bid ?b]
-                         :in '[$ ?user]
-                         :where '[[?b beitrag/id ?bid]
-                                  [?b beitrag/user ?user]]} db user)))
+                   (q {:find '[?user ?bid ?b]
+                       :in '[$ ?username]
+                       :where '[[?b :beitrag/id ?bid]
+                                [?b :beitrag/user ?user]
+                                [?user :user/name ?username]]} db user)))
 
-(defn qbeitragvonmatch ([db pattern]
-                     (q {:find '[?user]
-                         :in '[$ ?pattern]
-                         :where '[[?b beitrag/id ?bid]
-                                  [(fulltext $ :beitrag/user ?pattern) [[?b ?user]]
-                                   ]]} db pattern)))
-
-(defn qantworten
-  ([db]
-     (q {:find '[?a ?b]
-         :where '[[?b :beitrag/antworten ?a]]} db))
-  ([db bid]
-     (q {:find '[?a ?bid]
-         :in '[$ ?id]
-         :where '[[?b :beitrag/id ?id]
-                  [?a :beitrag/id ?bid]
-                  [?b :beitrag/antworten ?a]]} db bid)))
+(defn qbeitragvonmatch
+  ([db pattern]
+   (q {:find '[?user]
+       :in '[$ ?pattern]
+       :where '[[?b beitrag/id ?bid]
+                [(fulltext $ :beitrag/user ?pattern) [[?b ?user]]]]} db pattern)))
 
 (def parent-rule '[[[parent ?b1 ?b2]
                     [?b1 :beitrag/antworten ?b2]]
